107dfdeab7b8bd7a2e9e975cd9cf328472189040
juliocesar744 <jc-rl2@hotmail.com>
2022-09-24 21:16:09 -0300

    toDoList

---
+++ toDoList/src/components/Tasks.tsx
@@ -0,0 +1,35 @@
+import { ClipboardText } from 'phosphor-react';
+import { useState } from 'react';
+import styles from './Tasks.module.css'
+
+const [tasks, setTasks] = useState([]);
+
+function Tasks() {
+    return (
+        <>
+            <div className={styles.tarefas}>
+                <div className={styles.tarefasCriadas}>
+                        <p style={{color: "#4EA8DE"}}>Tarefas criadas</p>
+                        <div className={styles.qtd}>
+                            <p>0</p>
+                        </div>
+                    
+                    <div className={styles.tarefasConcluidas}>
+                        <p style={{color: "#8284FA"}}>Concluídas</p>
+                        <div className={styles.qtd}>
+                            <p>0</p>
+                        </div>
+                    </div>
+                </div>
+                {tasks.length === 0}
+                <div className={styles.semTarefas}>
+                    <ClipboardText color={"#808080"} size={47} />
+                    <p><strong>Você ainda não tem tarefas cadastradas</strong></p>
+                    <p>Crie tarefas e organize seus itens a fazer</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Tasks;
